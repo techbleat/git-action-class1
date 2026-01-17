0d30cef25ba861a7f05cbcaf517d52e1127d4e40
Olusegun <shegoj@yahoo.com>
2026-01-17 21:10:08 +0000

    init

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+    }   
+}
+
+provider "aws" {
+    region = "eu-west-1"
+}
+
+rerource "aws_instance" "new-node" {
+    ami           = "ami-03e091ef64f3907f8" # Amazon Linux 2 AMI
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "New Node"
+    }
+}
